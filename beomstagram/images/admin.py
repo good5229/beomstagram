b70a9d00811b9fb03b31ea27214ffc347fbd9cef
beommaker <beom.maker@gmail.com>
2018-01-08 17:15:10 +0900

    feat(beomstagram): customize-admin
    https://app.asana.com/0/513674312489999/513674312490018

--- beomstagram/images/admin.py
+++ beomstagram/images/admin.py
@@ -5,14 +5,49 @@ from . import models
 
 @admin.register(models.Image)
 class ImageAdmin(admin.ModelAdmin):
-    pass
+
+    list_display_links = (
+        'location',
+        'caption',
+    )
+
+    search_fields = (
+        'location',
+    )   
+
+    list_filter = (
+        'location',
+        'creator',
+    )
+
+    list_display = (
+        'creator',
+        'file',
+        'location',
+        'caption',
+        'created_at',
+        'updated_at',
+    )
 
 
 @admin.register(models.Like)
 class LikeAdmin(admin.ModelAdmin):
-    pass
+
+    list_display = (
+        'creator',
+        'image',
+        'created_at',
+        'updated_at',
+    )
 
 
 @admin.register(models.Comment)
 class CommnetAdmin(admin.ModelAdmin):
-    pass
+
+    list_display = (
+        'message',
+        'creator',
+        'image',
+        'created_at',
+        'updated_at',
+    )
