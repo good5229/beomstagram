35117c725468c3a2546464631fabfcca99725d25
beommaker <beom.maker@gmail.com>
2018-01-11 19:49:48 +0900

    feat(beomstagram): add-like-image
    https://app.asana.com/0/513674312489999/519210615256805

--- beomstagram/images/urls.py
+++ beomstagram/images/urls.py
@@ -7,4 +7,18 @@ urlpatterns = [
         view=views.Feed.as_view(),
         name='feed',
     ),
+    url(
+        regex=r'(?P<image_id>\w+)/like/',
+        view=views.LikeImage.as_view(),
+        name='like_image',
+    )
 ]
+
+
+# /image/3/like
+# /image/3/unlike
+
+# 0 create the url and the view
+# 1 take the id from the url
+# 2 we want to find an image with thid id
+# 3 we want to create a like for that image
