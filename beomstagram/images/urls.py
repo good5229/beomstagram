363427ef0391dc9dfab089dfd2c91bd3816a5c3c
beommaker <beom.maker@gmail.com>
2018-01-14 14:51:03 +0900

    feat(beomstagram): add-delete-comments
    https://app.asana.com/0/513674312489999/520988523960766

--- beomstagram/images/urls.py
+++ beomstagram/images/urls.py
@@ -8,22 +8,18 @@ urlpatterns = [
         name='feed',
     ),
     url(
-        regex=r'(?P<image_id>[0-9]+)/likes/',
+        regex=r'^(?P<image_id>[0-9]+)/likes/',
         view=views.LikeImage.as_view(),
         name='like_image',
     ),
     url(
-        regex=r'(?P<image_id>[0-9]+)/comment/',
+        regex=r'^(?P<image_id>[0-9]+)/comments/',
         view=views.CommentOnImage.as_view(),
         name='comment_image',
+    ),
+    url(
+        regex=r'^comments/(?P<comment_id>[0-9]+)/$',
+        view=views.Comment.as_view(),
+        name='comment'
     )
 ]
-
-
-# /image/3/like
-# /image/3/unlike
-
-# 0 create the url and the view
-# 1 take the id from the url
-# 2 we want to find an image with thid id
-# 3 we want to create a like for that image
