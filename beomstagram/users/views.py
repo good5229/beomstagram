e263baf3f815cd7e53ad49af9e77f2e42efc3d0d
beommaker <beom.maker@gmail.com>
2018-01-15 15:01:44 +0900

    feat(beomstagram): add-getting-user-profile
    https://app.asana.com/0/513674312489999/520988523960768

--- beomstagram/users/views.py
+++ beomstagram/users/views.py
@@ -23,7 +23,7 @@ class FollowUser(APIView):
 
         try:
             user_to_follow = models.User.objects.get(id=user_id)
-        except models.User.DoesNoteExist:
+        except models.User.DoesNotExist:
             return Response(status=status.HTTP_404_NOT_FOUND)
 
         user.following.add(user_to_follow)
@@ -41,7 +41,7 @@ class UnFollowUser(APIView):
 
         try:
             user_to_follow = models.User.objects.get(id=user_id)
-        except models.User.DoesNoteExist:
+        except models.User.DoesNotExist:
             return Response(status=status.HTTP_404_NOT_FOUND)
 
         user.following.remove(user_to_follow)
@@ -49,3 +49,17 @@ class UnFollowUser(APIView):
         user.save()
 
         return Response(status=status.HTTP_200_OK)
+
+
+class UserProfile(APIView):
+
+    def get(self, request, username, format=None):
+
+        try:
+            found_user = models.User.objects.get(username=username)
+        except models.User.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+        serializer = serializers.UserProfileSerializer(found_user)
+
+        return Response(data=serializer.data, status=status.HTTP_200_OK)
