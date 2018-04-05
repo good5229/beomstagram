f92363fc67f2bd914379eef16117dedd7e85ef41
beom <beommaker@gmail.com>
2018-04-05 09:43:04 +0900

    feat(frontend): install-redux-thunk-for-middlewares

--- frontend/src/redux/configureStore.js
+++ frontend/src/redux/configureStore.js
@@ -1,11 +1,16 @@
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import thunk from "redux-thunk";
 import users from 'redux/modules/users';
 
+const middlewares = [thunk, ];
 
 const reducer = combineReducers({
     users
 });
 
-let store = initialState => createStore(reducer);
+
+
+let store = initialState => 
+    createStore(reducer, applyMiddleware(...middlewares));
 
 export default store();
