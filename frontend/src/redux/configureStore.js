e4b419c9dc79347a143aa4aa0c841f33be3679b5
good5229 <good5229@naver.com>
2018-04-17 15:47:00 +0900

    feat(redux):install-reactotron

--- frontend/src/redux/configureStore.js
+++ frontend/src/redux/configureStore.js
@@ -4,6 +4,7 @@ import { routerReducer, routerMiddleware } from "react-router-redux";
 import createHistory from "history/createBrowserHistory";
 import thunk from "redux-thunk";
 import users from "redux/modules/users";
+import Reactotron from "ReactotronConfig";
 
 const env = process.env.NODE_ENV;
 
@@ -21,8 +22,15 @@ const reducer = combineReducers({
   routing: routerReducer
 });
 
-let store = initialState =>
+let store;
+
+if(env === "development"){
+  store = initialState =>
+  Reactotron.createStore(reducer, applyMiddleware(...middlewares));
+} else {
+  store = initialState =>
   createStore(reducer, applyMiddleware(...middlewares));
+}
 
 export { history };
 
