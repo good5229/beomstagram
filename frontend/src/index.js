5e0d66c291be23788c1bb64587aee2f60900e9dd
good5229 <good5229@naver.com>
2018-04-17 10:44:09 +0900

    fix(react):recreate-frontend-app

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,14 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import store from 'redux/configureStore';
-import 'index.css';
-import App from 'App';
+import './index.css';
+import App from './App';
 
-store.dispatch({ type: "FOR-TEST"});
-
-ReactDOM.render(
-    <Provider store={store}>
-        <App />
-    </Provider>,
-    document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
