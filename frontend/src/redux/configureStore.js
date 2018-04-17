5f7e748bc2da6efd3063b6491660c85b87dd1bd7
good5229 <good5229@naver.com>
2018-04-17 15:21:36 +0900

    feat(redux):syncing-react-router-with-redux

---
+++ frontend/src/redux/configureStore.js
@@ -0,0 +1,29 @@
+
+import { combineReducers, createStore, applyMiddleware } from "redux";
+import { routerReducer, routerMiddleware } from "react-router-redux";
+import createHistory from "history/createBrowserHistory";
+import thunk from "redux-thunk";
+import users from "redux/modules/users";
+
+const env = process.env.NODE_ENV;
+
+const history = createHistory();
+
+const middlewares = [thunk, routerMiddleware(history)];
+
+if (env === "development") {
+  const { logger } = require("redux-logger");
+  middlewares.push(logger);
+}
+
+const reducer = combineReducers({
+  users,
+  routing: routerReducer
+});
+
+let store = initialState =>
+  createStore(reducer, applyMiddleware(...middlewares));
+
+export { history };
+
+export default store();
